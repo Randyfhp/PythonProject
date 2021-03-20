d9cce2e895de98d2681e3b8f1418196de0950ea8
Randyfhp <Randyfhp@163.com>
2021-03-20 17:13:54 +0800

    Update BaseRequest.py
    修改请求基类

--- wxWorkRobot/network/BaseRequest.py
+++ wxWorkRobot/network/BaseRequest.py
@@ -11,16 +11,22 @@ class BaseRequest(object):
         return app
 
     def request_get(self, app, header=None, params=None):
+        return self.get(self.get_url(app), header, params)
+
+    def get(self, url, header=None, params=None):
         with requests.request("GET", self.get_url(app), params=params, headers=header) as res:
-            print(params, header)
+            print(params, header, sep='\r\n')
             print(res.url, res.status_code)
             return self.get_json(res)
 
-    def request_post(self, app, header=None, params=None, data=None):
+    def request_post(self, app, headers=None, params=None, data=None, **kwargs):
         if isinstance(data, dict):
             data = json.dumps(data)
-        with requests.request("POST", self.get_url(app), params=params, headers=header, data=data) as res:
-            print(header, params, data)
+        return self.post(self.get_url(app), headers, params, data, **kwargs)
+
+    def post(self, url, headers=None, params=None, data=None, **kwargs):
+        with requests.request("POST", url, params=params, headers=headers, data=data, **kwargs) as res:
+            print(headers, params, data, sep='\r\n')
             print(res.url, res.status_code)
             return self.get_json(res)
 
@@ -31,5 +37,4 @@ class BaseRequest(object):
             return res.json()
         except Exception as e:
             print(e.args)
-        finally:
             return {}
